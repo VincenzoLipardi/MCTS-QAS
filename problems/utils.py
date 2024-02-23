e99d14949195f499c73aa3bea96e15495b6b73a3
Vincenzo <90111277+VincenzoLipardi@users.noreply.github.com>
2024-02-23 14:31:30 +0100

    cleaning

--- problems/utils.py
+++
@@ -1,9 +0,0 @@
-def get_parameters(quantum_circuit):
-    parameters = []
-    # Iterate over all gates in the circuit
-    for instr, qargs, cargs in quantum_circuit.data:
-
-        # Extract parameters from gate instructions
-        if len(instr.params) > 0:
-            parameters.append(instr.params[0])
-    return parameters
